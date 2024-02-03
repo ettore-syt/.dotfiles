c3276e21c38986803237ca24ff895689f594d438
Ettore Mihaili <ettore@superyachttimes.com>
2024-02-03 01:20:57 +0100

    catpuccin statusbar changes and persist undo

--- .config/nvim/lua/hkt/options.lua
+++ .config/nvim/lua/hkt/options.lua
@@ -16,3 +16,9 @@ opt.updatetime = 50
 
 opt.colorcolumn = '80'
 
+-- persist undo across sessions
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
